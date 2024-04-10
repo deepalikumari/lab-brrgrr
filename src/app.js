05c7579520e871d9bc004dc1d3516730f3db8d16
Deepali kumari <deepalikumari1234@gmail.com>
2024-04-10 16:37:56 +0530

    did task

--- src/app.js
+++ src/app.js
@@ -4,23 +4,23 @@
 //Initial price of the burger
 var wholeWheatBun = 10;
 
-//Ingredients of the burger along with the price
+//ingrs of the burger along with the price
 // Clue: the name is same as the textcontent of the button. Will be useful later on :)
-var ingredients = {
+var ingrs = {
   Patty: 80,
   Cheese: 10,
   Tomatoes: 20,
   Onions: 20,
-  Lettuce: 20
+  Lettuce: 20,
 };
 
-//Current state of the ingredients in the burger
+//Current state of the ingrs in the burger
 var state = {
   Patty: true,
   Cheese: true,
   Tomatoes: true,
   Onions: true,
-  Lettuce: true
+  Lettuce: true,
 };
 
 // This function renders the entire screen everytime the state changes accordingly
@@ -31,7 +31,7 @@ function renderAll() {
   renderOnions();
   renderLettuce();
   renderButtons();
-  renderIngredientsBoard();
+  renderingrsBoard();
   renderPrice();
 }
 
@@ -47,18 +47,42 @@ function renderPatty() {
 
 function renderCheese() {
   //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese = document.querySelector("#cheese");
+  if (state.Cheese) {
+    cheese.style.display = "inherit";
+  } else {
+    cheese.style.display = "none";
+  }
 }
 
 function renderTomatoes() {
   //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let tomatoes = document.querySelector("#tomato");
+  if (state.Tomatoes) {
+    tomatoes.style.display = "inherit";
+  } else {
+    tomatoes.style.display = "none";
+  }
 }
 
 function renderOnions() {
   //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onions = document.querySelector("#onion");
+  if (state.Onions) {
+    onions.style.display = "inherit";
+  } else {
+    onions.style.display = "none";
+  }
 }
 
 function renderLettuce() {
   //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector("#lettuce");
+  if (state.Lettuce) {
+    lettuce.style.display = "inherit";
+  } else {
+    lettuce.style.display = "none";
+  }
 }
 
 document.querySelector(".btn-patty").onclick = function () {
@@ -68,22 +92,69 @@ document.querySelector(".btn-patty").onclick = function () {
 
 // Trial 2 - Setup event listener for the cheese button
 
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the tomatoes button
 
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the onion button
 
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+};
 
 // Trial 2 - Setup event listener for the lettuce button
 
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+};
 
 //Challenge 1 - Add/Remove the class active to the buttons based on state
 
+function renderButtons() {
+  for (let ingr in state) {
+    let button = document.querySelector(".btn-" + ingr.toLowerCase());
+    if (state[ingr]) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  }
+}
 
 //Challenge 2 - Render only the items selected in the ingredients board based on the state
+
+function renderingrsBoard() {
+  let eachingr = document.querySelectorAll(".items");
+  for (let i = 0; i<5; i++) {
+    if (state[eachingr[i].textContent]) {
+      eachingr[i].style.display = "inherit";
+    } else {
+      eachingr[i].style.display = "none";
+    }
+  }
+}
 
 
 //Judgement 1
 //In the p element having price-details as the class, display the calculated
-//price based on ingredients
+//price based on ingrs
+
+function renderPrice() {
+  let TP = wholeWheatBun;
+  for (let ingr in state) {
+    if (state[ingr]) {
+      TP += ingrs[ingr];
+    }
+  }
+  document.querySelector(".price-details").textContent = "INR " + TP;
+}
